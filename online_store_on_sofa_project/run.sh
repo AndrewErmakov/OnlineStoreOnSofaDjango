77f940dac6e3a602286f9c3b3f3b867f436707cf
AndrewErmakov <andrew.67@list.ru>
2022-06-24 23:50:06 +0500

    feat(OSSD-4): add nginx conf

--- online_store_on_sofa_project/run.sh
+++ online_store_on_sofa_project/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
-python manage.py runserver 0.0.0.0:8000
+
+gunicorn online_store_on_sofa_project.wsgi:application --bind 0.0.0.0:8000
